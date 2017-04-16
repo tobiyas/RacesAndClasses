e69ec2494582d32c92714045e8ebe550103bc9b2
Alex <squishybanana04@users.noreply.github.com>
2017-04-16 17:50:52 -0400

    Switch case to make it more readable

--- src/main/java/de/tobiyas/racesandclasses/util/traitutil/TraitStringUtils.java
+++ src/main/java/de/tobiyas/racesandclasses/util/traitutil/TraitStringUtils.java
@@ -20,19 +20,17 @@ public class TraitStringUtils {
 	public static double getNewValue(double oldDmg, String operation, double value){
 		double newDmg = 0;
 		
-		if(operation.equals("+")){
-			newDmg = oldDmg + value;
-		}else
-		
-		if(operation.equals("-")){
-			newDmg = oldDmg - value;
-		}else
-		
-		if(operation.equals("*")){
-			newDmg = oldDmg * value;
-		}else{
-			newDmg = oldDmg * value;
-		}
+		switch (operation) {
+			case "+":
+				newDmg = oldDmg + value;
+				break;
+			case "-":
+				newDmg = oldDmg - value;
+				break;
+			case "*":
+				newDmg = oldDmg * value;
+			default:
+				newDmg = oldDmg * value
 		
 		
 		if(newDmg < 0) newDmg = 0;
